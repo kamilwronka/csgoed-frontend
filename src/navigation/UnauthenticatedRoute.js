e37ce9a45fbe00fe718301e40be78b37398601c9
Kamil <kamilwronka7@gmail.com>
2020-01-14 17:23:26 +0100

    fixed redirect

--- src/navigation/UnauthenticatedRoute.js
+++ src/navigation/UnauthenticatedRoute.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
+import { parse } from "qs";
 
 export default ({ children, appProps, ...rest }) => (
   <Route
     {...rest}
-    render={props =>
-      !appProps.isAuthorized ? children : <Redirect to="/dashboard/home" />
-    }
+    render={props => {
+      const parsedQuery = parse(props.location.search.slice(1));
+
+      return !appProps.isAuthorized ? (
+        children
+      ) : (
+        <Redirect to={parsedQuery.redirect} />
+      );
+    }}
   />
 );
