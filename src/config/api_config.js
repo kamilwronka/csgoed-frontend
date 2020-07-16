a3d5f04ebf36ec12ffab21c08711e0bb41364f54
Kamil <kamilwronka7@gmail.com>
2020-07-16 18:43:03 +0200

    testing kube deploy pipeline

--- src/config/api_config.js
+++ src/config/api_config.js
@@ -1,7 +1,9 @@
 export default {
   API_URL: "http://192.168.8.130:4000",
-  AUTH_SERVICE_URL: "https://dev.api.kamilwronka.dev/auth-service",
-  USER_SERVICE_URL: "https://dev.api.kamilwronka.dev/auth-service",
+  AUTH_SERVICE_URL:
+    process.env.REACT_APP_AUTH_SERVICE_URL || "http://localhost:4000",
+  USER_SERVICE_URL:
+    process.env.REACT_APP_USER_SERVICE_URL || "http://localhost:4000",
   // API_URL: "https://api.csgoed.com",
   DEFAULT_AUTH_HEADER: "Authorization",
 };
